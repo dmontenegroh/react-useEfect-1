2ed5778150be2700dec77f44a6b608051d821e0a
dmontenegroh <dmontenegro.top@gmail.com>
2024-03-22 17:18:59 -0300

    creando useFetch con useCallback

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,80 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useCallback, useEffect } from "react";
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useFetch } from "./hooks/useFetch";
+
+// const fetchData = async (setUsers) => {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const data = await res.json();
+
+//   setUsers(data);
+// };
+
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  // const [users, setUsers] = useState(null);
+
+  console.log("app");
+
+  const { data, loading, error } = useFetch(
+    "https://jsonplaceholder.typicode.com/userss"
+  );
+
+  console.log(data);
+  // const fetchData = useCallback(async () => {
+  //   try {
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/users"
+  //     );
+  //     if (!response.ok) {
+  //       throw "Error al conectar la API";
+  //     }
+
+  //     const data = await response.json();
+  //     setusers(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, []);
+
+  // useEffect(() => {
+  //   // fetchData(setUsers);
+  // }, []);
+  // }, [fetchData]);
+
+  //  SIN ASYNC AWAIT
+  // useEffect(() => {
+  //   console.log("use effect");
+
+  //   // forma nativa para hacer peticiones http
+  //   fetch("https://jsonplaceholder.typicode.com/users")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setusers(data);
+  //     });
+  // }, []); // <- si [] se pasa vacio este se ejecuta una sola vez
+  // // }, [counter]); // <- [] : array de dependencias, lo que haya ahí adentro es lo que ejecutará dicho useEffect
+
+  if (loading) return <div>Cargando...</div>;
+
+  if (error) return <p>{error}</p>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>
+        useEffect{" "}
+        <button onClick={() => setCounter(counter + 1)}>
+          Counter: {counter}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <ul>
+          {data.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
